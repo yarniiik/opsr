d17931dc3955b1776efb19198ac72298f19d04dc
yarniiik <154729853+yarniiik@users.noreply.github.com>
2023-12-24 16:49:18 +0700

    My first commit

---
+++ pr1/ghfrnbr/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ghfrnbr
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("x: ");
+            var x = double.Parse(Console.ReadLine());
+            var y = 7.0 * Math.Pow(x, 2) + 3.0 * x + 6.0;
+            Console.WriteLine($"y: {y}");
+            Console.ReadKey();
+        }
+    }
+}
